73cc983aa6a57b514f61dbb9495d39e2f33fdae1
Ana Belen Ferreira <ana@pcdeana.localdomain>
2020-04-25 22:54:13 +0200

    add docs

---
+++ docs/precache-manifest.d273ecb8c05c8bc9c91bda07e4404b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fbe098b57cc22fa3a6200e240aafcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df782462d71524b1481d",
+    "url": "/static/css/main.a732fd4d.chunk.css"
+  },
+  {
+    "revision": "1221d938b7e8c08a65d2",
+    "url": "/static/js/2.fd08997f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd08997f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df782462d71524b1481d",
+    "url": "/static/js/main.ead1918d.chunk.js"
+  },
+  {
+    "revision": "827be3b8f3d7af2af35e",
+    "url": "/static/js/runtime-main.0846a844.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/logo.75509f2e.png"
+  }
+]);
